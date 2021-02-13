3f423acaf720c8f9e4285b7043edd55becd303d5
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-02-13 19:54:49 +0100

    init react-native-tts with test

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {View, Text} from 'react-native';
+import Tts from 'react-native-tts';
 
 export default function App() {
+  useEffect(() => {
+    Tts.speak('Hello, world ! this is a text to speech Test');
+  }, []);
   return (
     <View>
       <Text>CHATBOT</Text>
