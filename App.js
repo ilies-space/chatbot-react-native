1950d870a8350c92bcc3b0986a1f2fc5a8018eab
ilies ouldmenouer <63064471+ilyasxdz@users.noreply.github.com>
2021-02-13 20:38:41 +0100

    implimenting LottieView loader

--- App.js
+++ App.js
@@ -1,11 +1,26 @@
 import React, {useState} from 'react';
 import {View, Button, TextInput} from 'react-native';
 import Tts from 'react-native-tts';
+import LottieView from 'lottie-react-native';
 
 export default function App() {
   const [Textinit, setTextinit] = useState('');
   return (
-    <View>
+    <View
+      style={{
+        justifyContent: 'center',
+        // alignItems: 'center',
+      }}>
+      {/* //animation */}
+      <View
+        style={{
+          height: 200,
+          width: '100%',
+          alignItems: 'center',
+        }}>
+        <LottieView source={require('./asset/siriLoader.json')} autoPlay loop />
+      </View>
+
       <TextInput
         value={Textinit}
         onChangeText={(value) => {
